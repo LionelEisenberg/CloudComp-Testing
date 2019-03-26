ef53093edeae03a2062ec457004fae9c6971ff3b
LionelEisenberg <lionel.eisenberg@gmail.com>
2019-03-26 15:22:58 -0400

    added argument parsing in file

--- main_file.py
+++ main_file.py
@@ -1,5 +1,14 @@
+### Imports
+import sys
+
+### Declare variables
+NUM_ARGS = 0
+
 def main():
-  print("Hello World!")
+  arguments = sys.argv
+  if len(arguments) != NUM_ARGS:
+      print("Error: The number of arguments given does not satisfy the required number")
+      return ""
 
 if __name__== "__main__":
   main()
