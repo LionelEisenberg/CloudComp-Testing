74647c8e744d8768fcf8384fe5f1767a357a10ed
LionelEisenberg <lionel.eisenberg@gmail.com>
2019-03-26 15:30:00 -0400

    created backbone for functions to be swapped into

--- main_file.py
+++ main_file.py
@@ -2,14 +2,25 @@
 import sys
 
 ### Declare variables
-NUM_ARGS = 0
+NUM_ARGS = 2
+
+def sorting_func_a():
+	return
+
+def sorting_func_b():
+	return
 
 def main():
   arguments = sys.argv
   if len(arguments) != NUM_ARGS:
-      print("Error: The number of arguments given does not satisfy the required number")
-      return 
+	  print("Error: The number of arguments given does not satisfy the required number")
+	  return
 
+  sorting_func = arguments[1]
+  if sorting_func == "a":
+	  return sorting_func_a()
+  elif sorting_func == "b":
+	  return sorting_func_b()
 
 if __name__== "__main__":
   main()
