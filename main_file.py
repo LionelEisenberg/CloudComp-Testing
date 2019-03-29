b8368c4e0bf2ef4ac1d81ff0dac2eb7f2cca4eef
LionelEisenberg <lionel.eisenberg@gmail.com>
2019-03-29 13:01:52 -0400

    wrote function to copy file ocntents into array

--- main_file.py
+++ main_file.py
@@ -19,15 +19,20 @@ def sorting_func_c(array):
 
 def main():
   arguments = sys.argv
+
+  # Check if the number of arguments is accurate to what we are expecting
   if len(arguments) != NUM_ARGS:
 	  print("Error: The number of arguments given does not satisfy the required number")
 	  return
 
   sorting_func = arguments[1]
+
   filename = arguments[2]
-  file = open(filename, 'r')
-  for line in file:
-	  print(file)
+  with open(filename) as f:
+    array = f.readlines()
+
+  array = [i.strip() for i in content]
+
   if sorting_func == "a":
 	  return sorting_func_a(array)
   elif sorting_func == "b":
