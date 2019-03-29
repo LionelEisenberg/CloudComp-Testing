a8492936c8eb6816b18d7af9d84c4f469f7d476b
LionelEisenberg <lionel.eisenberg@gmail.com>
2019-03-29 12:53:45 -0400

    created file manipulation

--- main_file.py
+++ main_file.py
@@ -4,13 +4,18 @@ import sys
 ### Declare variables
 NUM_ARGS = 2
 
-def sorting_func_a():
+def sorting_func_a(file):
 	print("a function running")
 	return
 
-def sorting_func_b():
+def sorting_func_b(file):
 	print("b function running")
 	return
+
+def sorting_func_c(file):
+	print("c function running")
+	return
+
 
 def main():
   arguments = sys.argv
@@ -19,10 +24,13 @@ def main():
 	  return
 
   sorting_func = arguments[1]
+  filename = arguments[2]
   if sorting_func == "a":
-	  return sorting_func_a()
+	  return sorting_func_a(file)
   elif sorting_func == "b":
-	  return sorting_func_b()
+	  return sorting_func_b(file)
+  elif sorting_func == "c":
+	  return sorting_func_c(file)
   else:
 	  print("Error: The function you requested has not been implemented")
 	  return
